67ad34265693c8fdc425fd550c0af372edc04bd8
DeadSpawnZ <galeomh@hotmail.com>
2024-08-03 23:48:52 -0600

    feat: adding table to publishing template

--- comics/views/publishing.py
+++ comics/views/publishing.py
@@ -4,12 +4,22 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
 
-from comics.models import Publishing, Comic
+from comics.models import Publishing, Comic, Editorial, Printing
 
 
 def all(request):
     try:
         publishing_list = Publishing.objects.all().order_by("publishing_title").values()
+        for pub in publishing_list:
+            editorials = Editorial.objects.filter(publishing=pub["id"])
+            pub["editorials"] = [editorial.name for editorial in editorials]
+
+            printing = Printing.objects.filter(id=pub["printing_id"])
+            pub["print"] = printing[0]
+
+            first_editorial = editorials[0] if editorials else None
+            country_code = first_editorial.country.lower() if first_editorial else ""
+            pub["country"] = country_code
         context = {
             "publishing_list": publishing_list,
         }
