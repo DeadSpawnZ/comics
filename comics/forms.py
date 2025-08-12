58914775ceb8c1640cb62f1d96ced0e14471dab4
DeadSpawnZ <galeomh@hotmail.com>
2025-08-11 20:13:35 -0600

    feat(collection form): fetch the comics ordered

--- comics/forms.py
+++ comics/forms.py
@@ -19,7 +19,7 @@ class CollectionForm(forms.ModelForm):
         ]
 
     publishing = forms.ModelChoiceField(
-        queryset=Publishing.objects.all(),
+        queryset=Publishing.objects.all().order_by("publishing_title"),
         required=False,
         label="Publishing",
         help_text="Select a publishing to filter comics",
@@ -33,8 +33,10 @@ class CollectionForm(forms.ModelForm):
         if "publishing" in self.data:
             try:
                 publishing_id = int(self.data.get("publishing"))
-                self.fields["comic"].queryset = Comic.objects.filter(publishing_id=publishing_id)
+                self.fields["comic"].queryset = Comic.objects.filter(publishing_id=publishing_id).order_by("number")
             except (ValueError, TypeError):
                 pass
         elif self.instance.pk and self.instance.comic:
-            self.fields["comic"].queryset = Comic.objects.filter(publishing=self.instance.comic.publishing)
+            publishing_instance = self.instance.comic.publishing
+            self.fields["comic"].queryset = Comic.objects.filter(publishing=publishing_instance).order_by("number")
+            self.initial["publishing"] = publishing_instance
